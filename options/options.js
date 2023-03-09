b47bb99baaac716cf318b69f6c17192dc3b56f65
Crowdbotics <team@crowdbotics.com>
2023-03-09 13:24:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-fire-39294",
+  url: "https://ancient-fire-39294.botics.co",
+  api: "https://ancient-fire-39294.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
